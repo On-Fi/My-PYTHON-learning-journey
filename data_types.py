6204202c477edf60e0aca75823db8d3e5fa0f83b
onur fistikci <onurfistikci@mail.de>
2024-01-10 12:27:28 +0100

    add data_types.py and edit the readme

---
+++ data_types.py
@@ -0,0 +1,11 @@
+# Lesson 04 by Dave Gray
+
+# String data type
+
+# literal assignment
+frist = "Onur"
+last = "Fistik"
+
+print(type(frist)) # the type() function returns the type of an object | the output is <class 'str'>  'str' = string
+print(type(frist) == str) # == str is a Statment that means "the datatype is a string" | the output is a boolean in this case it is true 
+print(isinstance(frist, str)) # the isinstance() function returns True if a specified object is an instance of a specified object
