32f49cba0aeadf1b95b52863cf0e356908f95453
onur fistikci <onurfistikci@mail.de>
2024-01-11 20:24:30 +0100

    Update rock_paper_scissors.py

--- Lesson_05/rock_paper_scissors.py
+++ Lesson_05/rock_paper_scissors.py
@@ -1,3 +1,21 @@
 # Lesson 05 by Dave Gray
+import sys
+import random
+
+print("")
 print("")
 playerchoice = input("Enter... \n1 for Rock \n2 for Paper \n3 for Scissors:\n\n")
+
+player = int(playerchoice)
+
+if player < 1 | player > 3:
+    sys.exit("You must enter 1, 2 or 3 to play the game.")
+
+computerchoice = random.choice("123")  
+
+computer = int(computerchoice)
+
+print("")
+print("You chose " + playerchoice + ".")
+print("Python chose " + computerchoice + ".")
+print("")
