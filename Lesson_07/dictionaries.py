ca0eb5ed6649fcadc9bd97f5105f090c05162e68
onur fistikci <onurfistikci@mail.de>
2024-01-12 19:54:42 +0100

    Create dictionaries.py

---
+++ Lesson_07/dictionaries.py
@@ -0,0 +1,14 @@
+# Dictionaries
+band = {
+    "vocals" : "Plant",
+    "guitar" : "Page"
+}
+
+band2 = dict(vocals="Plant", guitar="Page")
+
+print(band)
+print("")
+print(band2)
+
+print(type(band))
+print(len(band))
